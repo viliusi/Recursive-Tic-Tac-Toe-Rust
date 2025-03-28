fbbde0427fb59287d4b73a9d1fcd0446e976d056
Vilius Ivanovas <ivanovasvilius@gmail.com>
2025-03-28 18:36:02 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Welcome, to Recursive Tic Tac Toe in Rust, by viliusi");
+    
+    // Start screen
+    //
+    // q = go back
+    // nums to focus on board section
+    // enter confirm
+    
+    
+
+
+
+}
